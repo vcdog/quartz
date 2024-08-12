f2a08274f9d43a37375c01e9f530a5bf89eae309
vcdog@163.com <vcdog@163.com>
2024-08-12 17:27:52 +0800

    Quartz sync: Aug 12, 2024, 5:27 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -14,7 +14,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
+    locale: "zh-CN",
     baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
